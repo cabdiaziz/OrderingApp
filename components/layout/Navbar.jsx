4db0d888dcf8671adbf8199c4cc42c4cb02ee64b
cabdiaziz <abdiaziiz1856@gmail.com>
2023-03-25 07:10:14 +0300

    updated navbar if isActive chage color else nope

--- components/layout/Navbar.jsx
+++ components/layout/Navbar.jsx
@@ -1,21 +1,35 @@
 import Link from "next/link";
-function NavbarItem({ url, title }) {
+import { useRouter } from "next/router";
+function NavbarItem({ url, title, isSelected }) {
   return (
-    <li className="mx-2 md: hover:underline hover:text-white">
+    <li
+      className={`mx-2 md: hover:underline hover:text-green-700 ${
+        isSelected ? "text-green-500" : "text-black"
+      }`}
+    >
       <Link href={url}>{title}</Link>
     </li>
   );
 }
 
 export default function Navbar() {
+  const router = useRouter();
   return (
-    <div className="flex md:flex-row flex-col justify-between text-3xl font-medium capitalize bg-sky-300 p-4">
-      <h1 className=" md:pl-32 pb-4">service ordering app</h1>
+    <div className="flex md:flex-row flex-col justify-between text-2xl font-medium capitalize bg-gray-200 rounded-md pt-4 pb-2 shadow-md border relative shadow-gray-300">
+      <h1 className=" md:pl-32 pb-4 font-black text-green-700">Ordering app</h1>
       <nav className="">
         <ul className="md:inline-flex">
-          <NavbarItem title="home" url="/" />
-          <NavbarItem title="orders" url="/orders" />
-          <NavbarItem title="new order" url="/orders/new" />
+          <NavbarItem title="home" url="/" isSelected={router.asPath == "/"} />
+          <NavbarItem
+            title="orders"
+            url="/orders"
+            isSelected={router.asPath == "/orders"}
+          />
+          <NavbarItem
+            title="add order"
+            url="/orders/new"
+            isSelected={router.asPath == "/orders/new"}
+          />
         </ul>
       </nav>
     </div>
