e6d5da4772d096b566ad98ba1811dc41ab18ab5f
cabdiaziz <abdiaziiz1856@gmail.com>
2023-03-26 17:21:10 +0300

    updated order list design

--- components/orders/OrdersList.jsx
+++ components/orders/OrdersList.jsx
@@ -2,7 +2,7 @@ import OrdersItem from "./OrdersItem";
 export default function OrdersList(props) {
   const { orders } = props;
   return (
-    <ul className="flex flex-col min-h-screen justify-center items-center bg-slate-100  ">
+    <ul className="flex flex-col min-h-screen pt-12 bg-slate-100  ">
       {orders.map((order) => (
         <OrdersItem
           key={order.id}
@@ -20,8 +20,9 @@ export default function OrdersList(props) {
   /* every order detail has a li tag */
 }
 {
-  /*//todo: 
-//*Feature :  quantity: no [x], 
-//? avatar orderedServiceName description date:2023-mar-10,  status btn
+  /*//todo list: 
+//*Feature :  
+
+//? Done : avatar, orderedName, description, date:2023-mar-10,  status, submit-button
 */
 }
